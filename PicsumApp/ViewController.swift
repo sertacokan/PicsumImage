9d94a2e05ed4fd7e4179f858da134f2647ecdaf7
Sertac Okan Celik <sertac.celik@yemeksepeti.com>
2021-05-16 01:00:37 +0300

    Initial Commit

---
+++ PicsumApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicsumApp
+//
+//  Created by Sertac Celik on 16.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
